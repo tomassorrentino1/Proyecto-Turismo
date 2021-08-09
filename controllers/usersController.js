879723936c77657cd2c1b04a6b15497ad7abee08
Tomas Sorrentino <tomas33sorrentino@gmail.com>
2021-08-09 20:21:03 -0300

    agregando logica de registro, se actualiza la base de datos con el nuevo usuario

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 const usersFilePath = path.join(__dirname, '../data/products.json');
-const users = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 
 
@@ -17,9 +21,16 @@ let usersController = {
 
     registerStore: (req, res) =>{
       const userToCreate = req.body;
-      const lastUser = users[users.length - 1]
-      userToCreate.id = lastUser.id + 1;
-      userToCreate.password = bcrypt.hashsync
+      
+      
+
+      userToCreate.password = bcrypt.hashSync(req.body.password, saltRounds)
+
+      users.push(userToCreate);
+      fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2))
+      return res.send(users);
+
+      
 
     }
 
--- routers/users.js
+++ routers/users.js
@@ -6,6 +6,7 @@ let usersController = require('../controllers/usersController');
 // Register
 
 router.get('/register', usersController.register);
+router.post('/register', usersController.registerStore);
 
 
 // Login
