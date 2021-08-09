8f7fa1e0c572b25c3407eaa5b93e5a72b4e41c9f
Tomas Sorrentino <tomas33sorrentino@gmail.com>
2021-08-09 19:09:41 -0300

    agregando sprint 5, vol 2

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, '../data/products.json');
+const users = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+
 let usersController = {
     login: function(req, res, next) {
         res.render('login');
@@ -5,7 +13,15 @@ let usersController = {
 
     register: function(req, res, next) {
       res.render('register');
-}
+},
+
+    registerStore: (req, res) =>{
+      const userToCreate = req.body;
+      const lastUser = users[users.length - 1]
+      userToCreate.id = lastUser.id + 1;
+      userToCreate.password = bcrypt.hashsync
+
+    }
 
 }
 
